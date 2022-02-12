6a21ce1f08ccfd804697eb067d9b131aee67e47e
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-13 03:49:16 +0900

    block class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-const sayHi = (name: string, age: number, gender: string): void => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}!!`);
-};
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
 
-sayHi("jun", 18, "male");
+const genesisBlock: Block = new Block(0, "7865456", "", "hello world", 123456);
+
+let blockChain: [Block] = [genesisBlock];
+
+console.log(blockChain);
