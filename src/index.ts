0ae82a8797a1714bad0669b268e047f92197b71a
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-13 04:27:58 +0900

    create block

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  };
+
   constructor(
     index: number,
     hash: string,
@@ -21,6 +33,31 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "7865456", "", "hello world", 123456);
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
 
-console.log(blockChain);
+const getBlockChain = (): Block[] => blockChain;
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+
+  return newBlock;
+};
+console.log(createNewBlock("hello"), createNewBlock("bye"));
